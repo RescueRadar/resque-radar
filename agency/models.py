4ad0a44f1044bdfe0032812166cebda3070f3d9b
Rachit Khurana <70265590+notnotrachit@users.noreply.github.com>
2023-09-23 09:59:44 +0530

    updated models

--- agency/models.py
+++ agency/models.py
@@ -16,11 +16,32 @@ class agency(models.Model):
     locality = models.CharField(max_length=50)
     city = models.CharField(max_length=50)
     state = models.CharField(max_length=50)
+    manpower = models.IntegerField()
+    volunteers = models.IntegerField()
+    category_of_calamity = models.CharField(max_length=50)
+    category_of_service = models.CharField(max_length=50)
+
 
     def __str__(self):
         return self.name
     
     class Meta:
         verbose_name_plural = "agencies"
+
+
+class requests(models.Model):
+    from_agency = models.ManyToManyField(agency, related_name='from_agency')
+    to_agency = models.ManyToManyField(agency, related_name='to_agency')
+    request = models.TextField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.request
+    
+    class Meta:
+        verbose_name_plural = "requests"
+
 
 
