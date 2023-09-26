971c6b7c372c0ed9d5af2d23ec4ac0148eaf4d0c
abhisek1221 <cherrymx317@gmail.com>
2023-09-26 22:53:36 +0530

    models updated for chatroom backend

--- agency/models.py
+++ agency/models.py
@@ -115,3 +115,32 @@ def new_agency(sender, instance, created, **kwargs):
     else:
         print("New agency not created")
         return False
+
+class chatRoom(models.Model):
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    participants = models.ManyToManyField(
+        User, related_name='participants', blank=True)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-updated', '-created']
+
+    def __str__(self):
+        return self.name
+
+
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(chatRoom, on_delete=models.CASCADE)
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-updated', '-created']
+
+    def __str__(self):
+        return self.body[0:50]
